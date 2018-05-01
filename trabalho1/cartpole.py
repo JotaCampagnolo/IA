666e54ec74d86cf31bb08d188f233234aed23037
João M. Campagnolo <jota.campagnolo@gmail.com>
2018-05-01 13:46:32 -0300

    Criando o arquivo base CartPole.py

---
+++ trabalho1/cartpole.py
@@ -0,0 +1,4 @@
+# UNIVERSIDADE FEDERAL DA FRONTEIRA SUL - UFFS, Campus Chapecó.
+# Ciência da Computação, 7a Fase, Matutino.
+# Trabalho 01 - CartPole usando Q-Learning.
+# Estudantes: João Marcos Campagnolo e Renan Casaca.
